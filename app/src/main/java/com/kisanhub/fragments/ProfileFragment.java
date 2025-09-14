dfe45d50ef83e00ab6b992eead33d45263bc59f6
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-09-14 18:45:09 +0530

    Update ProfileFragment.java

--- app/src/main/java/com/kisanhub/fragments/ProfileFragment.java
+++ app/src/main/java/com/kisanhub/fragments/ProfileFragment.java
@@ -5,7 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+import com.kisanhub.R;
 import com.kisanhub.databinding.FragmentProfileBinding;
 
 public class ProfileFragment extends Fragment {
@@ -16,6 +19,17 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // THIS IS THE NEW CODE THAT MAKES THE BUTTON WORK
+        binding.buttonViewFarms.setOnClickListener(v -> {
+            // This line tells the navigation system to go to the Farms List screen
+            NavHostFragment.findNavController(this).navigate(R.id.action_profileFragment_to_farmsListFragment);
+        });
     }
 
     @Override
