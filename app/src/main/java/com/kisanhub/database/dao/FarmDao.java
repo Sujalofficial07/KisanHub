64c5a3dcb34aadabd010464c832217654622b2c2
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-09-14 12:50:39 +0530

    Update FarmDao.java

--- app/src/main/java/com/kisanhub/database/dao/FarmDao.java
+++ app/src/main/java/com/kisanhub/database/dao/FarmDao.java
@@ -8,6 +8,9 @@ public interface FarmDao {
 
     @Delete
     void delete(Farm farm);
+    
+    @Query("SELECT COUNT(*) FROM farms WHERE userId = :userId")
+    LiveData<Integer> getFarmCountForUser(int userId);
 
     @Query("SELECT * FROM farms WHERE userId = :userId ORDER BY name ASC")
     LiveData<List<Farm>> getFarmsForUser(int userId);
