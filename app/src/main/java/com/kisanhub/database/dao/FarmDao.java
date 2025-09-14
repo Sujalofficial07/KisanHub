0f4a1a3cf7ad0616985cdfdadeaf0ec5e13535e7
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-09-14 12:34:22 +0530

    Create FarmDao.java

---
+++ app/src/main/java/com/kisanhub/database/dao/FarmDao.java
@@ -0,0 +1,14 @@
+@Dao
+public interface FarmDao {
+    @Insert
+    void insert(Farm farm);
+
+    @Update
+    void update(Farm farm);
+
+    @Delete
+    void delete(Farm farm);
+
+    @Query("SELECT * FROM farms WHERE userId = :userId ORDER BY name ASC")
+    LiveData<List<Farm>> getFarmsForUser(int userId);
+}
