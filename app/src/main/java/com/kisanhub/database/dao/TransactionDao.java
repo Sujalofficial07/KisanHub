bbbc927a55478e45fade59f7112da5501f50f443
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-09-14 12:52:14 +0530

    Update TransactionDao.java

--- app/src/main/java/com/kisanhub/database/dao/TransactionDao.java
+++ app/src/main/java/com/kisanhub/database/dao/TransactionDao.java
@@ -5,4 +5,13 @@ public interface TransactionDao {
 
     @Query("SELECT * FROM transactions WHERE farmId = :farmId ORDER BY date DESC")
     LiveData<List<Transaction>> getTransactionsForFarm(int farmId);
+    
+    @Query("SELECT SUM(amount) FROM transactions WHERE farmId IN (SELECT id FROM farms WHERE userId = :userId) AND type = 'income'")
+    LiveData<Double> getTotalIncomeForUser(int userId);
+
+    @Query("SELECT SUM(amount) FROM transactions WHERE farmId IN (SELECT id FROM farms WHERE userId = :userId) AND type = 'expense'")
+    LiveData<Double> getTotalExpensesForUser(int userId);
+    
+    @Query("SELECT * FROM transactions WHERE farmId = :farmId")
+    LiveData<List<Transaction>> getAllTransactionsForFarm(int farmId);
 }
