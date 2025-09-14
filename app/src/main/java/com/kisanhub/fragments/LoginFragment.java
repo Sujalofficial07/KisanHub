e0a927d84a065b21a4fbde5efe166c02be970fc8
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-09-14 16:50:03 +0530

    Update LoginFragment.java

--- app/src/main/java/com/kisanhub/fragments/LoginFragment.java
+++ app/src/main/java/com/kisanhub/fragments/LoginFragment.java
@@ -5,22 +5,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
-// You will need to create this binding class if it doesn't exist
-// import com.kisanhub.databinding.FragmentLoginBinding;
+import com.kisanhub.databinding.FragmentLoginBinding;
 
 public class LoginFragment extends Fragment {
 
-    // private FragmentLoginBinding binding;
+    private FragmentLoginBinding binding;
 
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // binding = FragmentLoginBinding.inflate(inflater, container, false);
-        // return binding.getRoot();
-        // For now, return a placeholder to allow compilation
-        return new View(getContext());
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
+        // Add ViewModel and click listeners here in a later step
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
