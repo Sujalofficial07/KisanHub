c20d50f2f851f82d0cd8c8da420010da94d1a438
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-09-14 16:49:08 +0530

    Update AppDatabase.java

--- app/src/main/java/com/kisanhub/database/AppDatabase.java
+++ app/src/main/java/com/kisanhub/database/AppDatabase.java
@@ -5,7 +5,8 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import com.kisanhub.database.dao.FarmDao;
-// Add other DAOs here
+import com.kisanhub.database.dao.TransactionDao;
+import com.kisanhub.database.dao.UserDao;
 import com.kisanhub.database.entity.Farm;
 import com.kisanhub.database.entity.Transaction;
 import com.kisanhub.database.entity.User;
@@ -15,8 +16,9 @@ import java.util.concurrent.Executors;
 @Database(entities = {User.class, Farm.class, Transaction.class}, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
 
-    // Add abstract methods for your DAOs
+    public abstract UserDao userDao();
     public abstract FarmDao farmDao();
+    public abstract TransactionDao transactionDao();
 
     private static volatile AppDatabase INSTANCE;
     private static final int NUMBER_OF_THREADS = 4;
