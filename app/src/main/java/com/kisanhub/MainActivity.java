071c5f3174dcad83da2e71935731d08fc4bc7ffb
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-09-14 17:01:59 +0530

    Update MainActivity.java

--- app/src/main/java/com/kisanhub/MainActivity.java
+++ app/src/main/java/com/kisanhub/MainActivity.java
@@ -4,10 +4,10 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // This layout contains the NavHostFragment, so we just need to set it.
         setContentView(R.layout.activity_main);
     }
 }
