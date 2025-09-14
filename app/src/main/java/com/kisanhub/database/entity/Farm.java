4f09970d8f9d5760af0d16b94eb94e2a73f21009
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-09-14 16:41:16 +0530

    Update Farm.java

--- app/src/main/java/com/kisanhub/database/entity/Farm.java
+++ app/src/main/java/com/kisanhub/database/entity/Farm.java
@@ -1,3 +1,9 @@
+package com.kisanhub.database.entity;
+
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
 @Entity(tableName = "farms", foreignKeys = @ForeignKey(entity = User.class,
         parentColumns = "id",
         childColumns = "userId",
