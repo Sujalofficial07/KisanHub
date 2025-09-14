32f49d26360bc49e0ad4162de3ca138ea19d28c0
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-09-14 17:09:15 +0530

    Update AddTransactionViewModel.java

--- app/src/main/java/com/kisanhub/viewmodels/AddTransactionViewModel.java
+++ app/src/main/java/com/kisanhub/viewmodels/AddTransactionViewModel.java
@@ -1,10 +1,20 @@
+package com.kisanhub.viewmodels;
+
+import android.app.Application;
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import com.kisanhub.database.entity.Transaction;
+import com.kisanhub.repositories.FarmRepository;
+
 public class AddTransactionViewModel extends AndroidViewModel {
-    private FarmRepository repository;
-    public AddTransactionViewModel(Application application) {
+    private final FarmRepository repository;
+
+    public AddTransactionViewModel(@NonNull Application application) {
         super(application);
         repository = new FarmRepository(application);
     }
+
     public void insert(Transaction transaction) {
-        repository.insertTransaction(transaction); // Add this method to the repository
+        repository.insertTransaction(transaction);
     }
 }
