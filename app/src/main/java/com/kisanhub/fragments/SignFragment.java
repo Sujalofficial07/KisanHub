0ac643a00dd59fb966f9167e8fd2c807ecaf4886
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-09-14 16:50:54 +0530

    Create SignFragment.java

---
+++ app/src/main/java/com/kisanhub/fragments/SignFragment.java
@@ -0,0 +1,26 @@
+package com.kisanhub.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import com.kisanhub.databinding.FragmentSignupBinding;
+
+public class SignupFragment extends Fragment {
+
+    private FragmentSignupBinding binding;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentSignupBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
