e990663dd2f6627748f46889f8e193019231261f
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-09-14 12:33:49 +0530

    Create UserDao.java

---
+++ app/src/main/java/com/kisanhub/database/dao/UserDao.java
@@ -0,0 +1,11 @@
+@Dao
+public interface UserDao {
+    @Insert
+    void insert(User user);
+
+    @Query("SELECT * FROM users WHERE username = :username AND password = :password LIMIT 1")
+    User login(String username, String password);
+
+    @Query("SELECT * FROM users WHERE username = :username LIMIT 1")
+    User getUser(String username);
+}
