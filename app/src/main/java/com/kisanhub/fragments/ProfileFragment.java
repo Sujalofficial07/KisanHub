7a0641d47a73c3c8cc4b5fb6e116f3cc0217cb85
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-09-14 16:51:46 +0530

    Update ProfileFragment.java

--- app/src/main/java/com/kisanhub/fragments/ProfileFragment.java
+++ app/src/main/java/com/kisanhub/fragments/ProfileFragment.java
@@ -5,19 +5,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
-// import com.kisanhub.databinding.FragmentProfileBinding;
+import com.kisanhub.databinding.FragmentProfileBinding;
 
 public class ProfileFragment extends Fragment {
-    
-    // private FragmentProfileBinding binding;
 
-    @Nullable
+    private FragmentProfileBinding binding;
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // return a placeholder to allow compilation
-        return new View(getContext());
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
