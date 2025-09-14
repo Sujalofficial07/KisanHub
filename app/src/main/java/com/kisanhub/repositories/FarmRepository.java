fac73ea3222cb5e9c9035feda1054840f03a4583
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-09-14 17:03:11 +0530

    Update FarmRepository.java

--- app/src/main/java/com/kisanhub/repositories/FarmRepository.java
+++ app/src/main/java/com/kisanhub/repositories/FarmRepository.java
@@ -1,3 +1,14 @@
+package com.kisanhub.repositories;
+
+import android.app.Application;
+import androidx.lifecycle.LiveData;
+import com.kisanhub.database.AppDatabase;
+import com.kisanhub.database.dao.FarmDao;
+import com.kisanhub.database.dao.TransactionDao;
+import com.kisanhub.database.entity.Farm;
+import com.kisanhub.database.entity.Transaction;
+import java.util.List;
+
 public class FarmRepository {
     private FarmDao farmDao;
     private TransactionDao transactionDao;
@@ -7,26 +18,19 @@ public class FarmRepository {
         farmDao = db.farmDao();
         transactionDao = db.transactionDao();
     }
-    
+
+    // Farm operations
     public void insertFarm(Farm farm) { AppDatabase.databaseWriteExecutor.execute(() -> farmDao.insert(farm)); }
     public void updateFarm(Farm farm) { AppDatabase.databaseWriteExecutor.execute(() -> farmDao.update(farm)); }
     public void deleteFarm(Farm farm) { AppDatabase.databaseWriteExecutor.execute(() -> farmDao.delete(farm)); }
     
+    // Transaction operations
+    public void insertTransaction(Transaction transaction) { AppDatabase.databaseWriteExecutor.execute(() -> transactionDao.insert(transaction)); }
 
-    // For Profile Dashboard
+    // LiveData getters
     public LiveData<Integer> getFarmCount(int userId) { return farmDao.getFarmCountForUser(userId); }
     public LiveData<Double> getTotalIncome(int userId) { return transactionDao.getTotalIncomeForUser(userId); }
     public LiveData<Double> getTotalExpenses(int userId) { return transactionDao.getTotalExpensesForUser(userId); }
-
-    // For Farms List
-    public LiveData<List<Farm>> getFarms(int userId) { return farmDao.getAllFarmsForUser(userId); }
-    
-    // Get transactions for a specific farm to calculate profit/loss
-    public LiveData<List<Transaction>> getTransactionsForFarm(int farmId) {
-        return transactionDao.getAllTransactionsForFarm(farmId);
-    }
-
-    public void insertFarm(Farm farm) {
-        AppDatabase.databaseWriteExecutor.execute(() -> farmDao.insert(farm));
-    }
+    public LiveData<List<Farm>> getFarms(int userId) { return farmDao.getFarmsForUser(userId); }
+    public LiveData<List<Transaction>> getTransactionsForFarm(int farmId) { return transactionDao.getTransactionsForFarm(farmId); }
 }
